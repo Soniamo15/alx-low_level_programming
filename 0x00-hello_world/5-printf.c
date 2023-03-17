5a1cda72a08490e9fdaba28bd4693fc7844630be
Sonia Ekuma <soniaekuma@gmail.com>
2023-03-17 05:14:30 -0700

    4-puts.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#inculde <stdio.h>
+/**
+ * main - A program tha print a line using the printf function
+ * Return: 0 (Success)
+ */
+int main(viod)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}	
