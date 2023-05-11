6c722c053ff0f75fbe4b0eb5f67853c12faf15e1
Sonia Ekuma <soniaekuma@gmail.com>
2023-05-11 02:39:43 -0700

    file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,7 +16,7 @@ char *create_buffer(char *file)
 	if (buffer == NULL)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
-			exit(99);
+		exit(99);
 	}
 
 	return (buffer);
@@ -49,9 +49,9 @@ void close_file(int fd)
  * Return: 0 on success.
  *
  * Description: If the argument count is incorrect - exit code 97.
- * If file_from does not exist or cannot be read - exit code 98.
- * If file_to cannot be created or written to - exit code 99.
- * If file_to or file_from cannot be closed - exit code 100.
+ *              If file_from does not exist or cannot be read - exit code 98.
+ *              If file_to cannot be created or written to - exit code 99.
+ *              If file_to or file_from cannot be closed - exit code 100.
  */
 int main(int argc, char *argv[])
 {
@@ -74,9 +74,9 @@ int main(int argc, char *argv[])
 		if (from == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
-					"Error: Can't read from file %s\n", argv[1]);
+			     "Error: Can't read from file %s\n", argv[2]);
 			free(buffer);
-			exit(98);
+			exit(99);
 		}
 
 		w = write(to, buffer, r);
@@ -84,7 +84,7 @@ int main(int argc, char *argv[])
 		if (to == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO,
-					"Error: Can't write to %s\n", argv[2]);
+			    "Error: Can't write to %s\n", argv[2]);
 			free(buffer);
 			exit(99);
 		}
